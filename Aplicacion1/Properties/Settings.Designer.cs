7261f49e39ec4a4e01b999673e60ff4cd0b91253
Emanuel Hernández Chavéz <automuestreador@outlook.com>
2024-12-27 21:35:54 -0600

    version de windows 7 final

--- Aplicacion1/Properties/Settings.Designer.cs
+++ Aplicacion1/Properties/Settings.Designer.cs
@@ -166,5 +166,17 @@ namespace Trapid.Properties {
                 this["señalgo"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public string delay1 {
+            get {
+                return ((string)(this["delay1"]));
+            }
+            set {
+                this["delay1"] = value;
+            }
+        }
     }
 }
--- Aplicacion1/Utilidades/Configuracion.cs
+++ Aplicacion1/Utilidades/Configuracion.cs
@@ -137,6 +137,7 @@ namespace Trapid.Utilidades
             MiConfig.resolucion = Properties.Settings.Default.resolucion;
             MiConfig.orientacion = Properties.Settings.Default.orientacion;
             MiConfig.señalgo = Properties.Settings.Default.señalgo;
+            MiConfig.delay1 = Properties.Settings.Default.delay1;
         }
 
         public static void GuardarConfiguracion()
@@ -154,7 +155,7 @@ namespace Trapid.Utilidades
             Properties.Settings.Default.resolucion= MiConfig.resolucion;
             Properties.Settings.Default.orientacion = MiConfig.orientacion;
             Properties.Settings.Default.señalgo = MiConfig.señalgo;
-            //Properties.Settings.Default.Save();
+            Properties.Settings.Default.Save();
 
 
         }
@@ -174,6 +175,7 @@ namespace Trapid.Utilidades
         public static string resolucion { get; set; } = "";
         public static string orientacion { get; set; } = "";
         public static string señalgo { get; set; } = "";
+        public static string delay1 { get; set; } = "";
     }
 }
 */
